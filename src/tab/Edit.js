6f3fe3065b2af95b86311ea7c4ddfc6d7bff00ad
NF Tushar <58363866+nftushar@users.noreply.github.com>
2023-10-17 14:58:58 +0600

    Update Edit.js
    InnerBlocks core/paragraph
    
    
    Signed-off-by: NF Tushar <58363866+nftushar@users.noreply.github.com>

--- src/tab/Edit.js
+++ src/tab/Edit.js
@@ -12,29 +12,25 @@ const Edit = (props) => {
 		clientId && setAttributes({ cId: clientId.substring(0, 10) }); // Set & Update clientId to cId
 	}, [clientId]);
 
-	return (
-		<>
-			<Settings attributes={attributes} setAttributes={setAttributes} />
-
-			<div className='wp-block-tcb-tab' id={`tcbTabbedContentTab-${clientId}`}>
-				<InnerBlocks
-					template={[
-						[
-							'core/heading',
-							{
-								content: `Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-								"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-								 qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"`,
-								style: {
-									typography: { fontSize: '15px' },
-								},
+	return <>
+		<Settings attributes={attributes} setAttributes={setAttributes} />
+		<div className='wp-block-tcb-tab' id={`tcbTabContentTab-${clientId}`}>
+			<InnerBlocks
+				template={[
+					[
+						'core/paragraph',
+						{
+							content: `In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available 
+								In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available`,
+							style: {
+								typography: { fontSize: '15px' },
 							},
-						],
-					]}
-				/>
-			</div>
-		</>
-	);
+						},
+					],
+				]}
+			/>
+		</div>
+	</>
 };
 
 export default Edit;
